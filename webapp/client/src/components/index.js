4b2141fa294404c1949c313a87edfca5809aa789
Francesco <56157402+BrunoFrancesco97@users.noreply.github.com>
2024-10-23 22:10:31 +0200

    Removal of old files

--- webapp/client/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Jumbotron } from "./jumbotron/jumbotron.component";
-export { default as Search } from "./search/search.component";
-export { default as FirstQueryResult } from "./first-query-result/first-query-result.component";
